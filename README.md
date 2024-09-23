9477dfc29ac51814ba71e19fa5c70d4d0ef47ba7
ckullo <ckullo@gmail.com>
2024-09-24 00:02:03 +0700

    20240923

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# todo
+# Change log
+## 2024.09.23
+### Front-End
+- install react-icons
+- update core-js@1.2.7 to core-js@^3
+- remove Add button
+- Add eventhandlers for keydown events to replace Add Button functionality
+- Change delete button text with font-awesome@6 trash icon
--- todo-frontend/src/components/AddTodo.js
+++ todo-frontend/src/components/AddTodo.js
@@ -1,26 +1,18 @@
 import React, { useState } from 'react';
 import { Form, Button, InputGroup } from 'react-bootstrap';
-import { FaRegFile } from 'react-icons/fa6';
+
 function AddTodo({ onAdd }) {
   const [title, setTitle] = useState('');
-  // const [description, setDescription] = useState('');
-  // const [priority, setPriority] = useState('');
-  // const [dueDate, setDueDate] = useState('');
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!title.trim()) return; // Prevent submission if title is empty
-    // if (text) {
-    //   onAdd(text);
-    //   setText('');
-    // }
+
     try {
       const newTodo = {
         title:title, 
-        // description:"", 
-        // completed: false, 
-        // priority: "Medium", 
-        // dueDate: dueDate
+
       };
 
       onAdd(newTodo);
@@ -46,33 +38,7 @@ function AddTodo({ onAdd }) {
           onChange={(e) => setTitle(e.target.value)}
           placeholder="Add a task"
         />
-        {/* <Form.Control
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          placeholder="Description"
-        />
-        <Form.Control
-          as="select"
-          value={priority}
-          onChange={e => {
-            console.log("e.target.value", e.target.value);
-            setPriority(e.target.value);
-          }}
-        >
-          <option>Select Priority</option>
-          <option value="High">High</option>
-          <option value="Medium">medium</option>
-          <option value="Low">Low</option>
-        </Form.Control>
-        <Form.Control
-          type="date"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-        /> */}
-        {/* <Button type="submit" variant="primary">
-          <FaRegFile />
-        </Button> */}
+        
       </InputGroup>
     </Form>
   );
